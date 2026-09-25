0e9fe6ed3d15efbe50da21cd89540cc6f91aa947
Krishnads210 <35193276+Krishnads210@users.noreply.github.com>
2026-09-25 07:44:56 +0530

    this merges Krishna

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 print("This is my first line of code")
 print("This code is coming from Hitesh")
 print("This code is from KVSRKM")
-<<<<<<< HEAD
 print("This is bascically our changes done by Krishna in number 4.")
 print("This is bascically our changes done by Krishna in number 5.")
 print("This is bascically our changes done by Krishna in number 6.")
-=======
 print("This new line is from KVSRKM")
->>>>>>> KVSRKM
